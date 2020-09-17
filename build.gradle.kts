66cedb16ae380d604c8e45422ca953ed7ff7832a
YukiLeafX <yukileafx@gmail.com>
2020-09-18 02:36:17 +0900

    upload

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import java.util.*
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+}
+
+group = "net.azisaba"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly("io.github.waterfallmc:waterfall-api:1.16-R0.4-SNAPSHOT")
+}
+
+tasks {
+    compileKotlin { kotlinOptions.jvmTarget = "1.8" }
+    compileTestKotlin { kotlinOptions.jvmTarget = "1.8" }
+
+    shadowJar {
+        relocate("kotlin", UUID.randomUUID().toString())
+        relocate("org.jetbrains.annotations", UUID.randomUUID().toString())
+
+        minimize()
+    }
+}
---
+++ src/main/kotlin/net/azisaba/yukibungeestats/YukiBungeeStats.kt
@@ -0,0 +1,41 @@
+package net.azisaba.yukibungeestats
+
+import net.md_5.bungee.api.ServerPing
+import net.md_5.bungee.api.event.PostLoginEvent
+import net.md_5.bungee.api.event.ProxyPingEvent
+import net.md_5.bungee.api.plugin.Listener
+import net.md_5.bungee.api.plugin.Plugin
+import net.md_5.bungee.event.EventHandler
+import java.net.InetSocketAddress
+import java.util.*
+
+class YukiBungeeStats : Plugin(), Listener {
+
+    private val pings = mutableMapOf<String, Int>().withDefault { 0 }
+    private val connects = mutableMapOf<UUID, Int>().withDefault { 0 }
+
+    override fun onEnable() {
+        proxy.pluginManager.registerListener(this, this)
+    }
+
+    @EventHandler
+    fun onPing(e: ProxyPingEvent) {
+        val ip = (e.connection.socketAddress as InetSocketAddress).address.hostAddress
+        pings[ip] = pings.getValue(ip).inc()
+    }
+
+    @EventHandler
+    fun onConnect(e: PostLoginEvent) {
+        val uuid = e.player.uniqueId
+        connects[uuid] = connects.getValue(uuid).inc()
+    }
+
+    @EventHandler
+    fun onPingForStats(e: ProxyPingEvent) {
+        val sample = arrayOf(
+            "Ping数: ${pings.size} (${pings.values.sum()})",
+            "接続数: ${connects.size} (${connects.values.sum()})"
+        ).map { ServerPing.PlayerInfo(it, UUID.randomUUID()) }.toTypedArray()
+        e.response.players.sample = sample
+    }
+}
